4102a882aa7179a8fe7afd9a9f379705e1377cd0
Manely Hamedani <manely.hamedani@icloud.com>
2025-01-14 08:36:02 +0330

    Modify fork() to call fork_core().

--- kernel/proc.c
+++ kernel/proc.c
@@ -355,65 +355,16 @@ struct proc *fork_core(void) {
 }
 
 // simple fork.
-// int
-// fork(void)
-// {
-//   struct proc *np = fork_core();
-
-//   if (np) {
-//     return np->pid;
-//   }
-
-//   return -1;
-// }
-
 int
 fork(void)
 {
-  int i, pid;
-  struct proc *np;
-  struct proc *p = myproc();
+  struct proc *np = fork_core();
 
-  // Allocate process.
-  if((np = allocproc()) == 0){
-    return -1;
+  if (np) {
+    return np->pid;
   }
 
-  // Copy user memory from parent to child.
-  if(uvmcopy(p->pagetable, np->pagetable, p->sz) < 0){
-    freeproc(np);
-    release(&np->lock);
-    return -1;
-  }
-  np->sz = p->sz;
-
-  // copy saved user registers.
-  *(np->trapframe) = *(p->trapframe);
-
-  // Cause fork to return 0 in the child.
-  np->trapframe->a0 = 0;
-
-  // increment reference counts on open file descriptors.
-  for(i = 0; i < NOFILE; i++)
-    if(p->ofile[i])
-      np->ofile[i] = filedup(p->ofile[i]);
-  np->cwd = idup(p->cwd);
-
-  safestrcpy(np->name, p->name, sizeof(p->name));
-
-  pid = np->pid;
-
-  release(&np->lock);
-
-  acquire(&wait_lock);
-  np->parent = p;
-  release(&wait_lock);
-
-  acquire(&np->lock);
-  np->state = RUNNABLE;
-  release(&np->lock);
-
-  return pid;
+  return -1;
 }
 
 
